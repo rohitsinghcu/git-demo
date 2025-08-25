2a6a1d7562c466a776213738359f74ded7d5aea8
pranavchougule511 <pranavchougule10@gmail.com>
2025-08-26 00:06:46 +0530

    Reapply "Feat(Homepage) : Add greeting and day description"
    This reverts commit 074768552fd09453701714d838209913b4049af3.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.          
         </p>
         <p className='subHeading'>
-          Hey, this is Git Session & today is Tuesday 26th Aug.
+          Hey, this is Git Session & today is Tuesday 26th August. <br/>
+          And it's a beautiful sunny Day.
         </p>
 
         <button>Click Here!</button>
