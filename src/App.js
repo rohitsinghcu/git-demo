9964497af355843de784c7f460975f44dd59e464
pranavchougule511 <pranavchougule10@gmail.com>
2025-08-25 22:55:31 +0530

    Feat(Homepage):Add new paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.          
+        </p>
+        <p>
+          Hey, this is Git Session.
         </p>
         <a
           className="App-link"
